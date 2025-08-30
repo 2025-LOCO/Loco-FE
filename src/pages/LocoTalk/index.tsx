64c54bb5814da39389fa48a3da591de3921fa841
DaheeJang <jecydahee77@gmail.com>
2025-08-30 13:25:44 +0900

    Chore: 로코톡톡페이지 초기구성

--- src/pages/LocoTalk/index.tsx
+++ src/pages/LocoTalk/index.tsx
@@ -1,7 +1,16 @@
+import LocationDropDown from "@/components/LocationDropDown";
+import * as S from "./styles";
+
 export default function LocoTalkPage() {
   return (
     <>
-      <div>로코톡톡</div>
+      <S.TalkSection>
+        <div>
+          <LocationDropDown />
+          <S.LocationIntroduce>지역에 대한 한 줄 설명</S.LocationIntroduce>
+        </div>
+        <S.Title>로코 문답</S.Title>
+      </S.TalkSection>
     </>
   );
 }
